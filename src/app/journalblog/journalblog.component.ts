b5b549bb04f4d73fdcce0fbad4cc36f1112f2aa7
SIM Chia Huey <sim.chiahuey@gmail.com>
2026-03-26 15:07:48 +0100

    Updated latest articles to JournalComponent

--- src/app/journalblog/journalblog.component.ts
+++ src/app/journalblog/journalblog.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as journalContent from '../../assets/content/journal-content.json';
-import { NgZone } from '@angular/core';
 
 @Component({
     selector: 'app-journalblog',
@@ -11,65 +10,30 @@ import { NgZone } from '@angular/core';
 export class JournalblogComponent implements OnInit {
   // The ().default accesses the actual JSON content, when you import it like a module, instead of HttpClient.
   contents: any = (journalContent as any).default;
-  currentPage = 1;
-  itemsPerPage = 9;  
+  
+  
   // Track current slide index for each content item
   currentSlideIndices: { [key: string]: number } = {};
 
-  // get reversedContents() {
-  //   return [...this.contents].reverse();
-  // }
+  get reversedContents() {
+    return [...this.contents].reverse();
+  }
 
-  constructor(private ngZone: NgZone) { }
+  constructor() { }
 
   ngOnInit(): void {
     // Initialize current slide index for each content item
-    // this.reversedContents.forEach((content: any) => {
-    //   this.currentSlideIndices[content.id] = 0;
-    // });
-  }
-
-  // Pagination methods
-  get paginatedContents(): any[] {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    return this.contents.slice(startIndex, startIndex + this.itemsPerPage);
-  }
-
-  get totalPages(): number {
-    return Math.ceil(this.contents.length / this.itemsPerPage);
-  }
-
-  get pages(): number[] {
-    return Array(this.totalPages).fill(0).map((x, i) => i + 1);
-  }
-
-  changePage(page: number): void {
-    if (page >= 1 && page <= this.totalPages) {
-      this.currentPage = page;
-      window.scrollTo(0, 0);
-    }
-  }
-
-  goToFirstPage(): void {
-    this.changePage(1);
-  }
-
-  goToLastPage(): void {
-    this.changePage(this.totalPages);
+    this.reversedContents.forEach((content: any) => {
+      this.currentSlideIndices[content.id] = 0;
+    });
   }
 
   // Carousel navigation methods
   nextSlide(contentId: string, imageCount: number): void {
-  this.ngZone.runOutsideAngular(() => {
     if (this.currentSlideIndices[contentId] < imageCount - 1) {
       this.currentSlideIndices[contentId]++;
-      // Use requestAnimationFrame for smooth updates
-      requestAnimationFrame(() => {
-        // DOM updates here
-      });
     }
-  });
-}
+  }
 
   prevSlide(contentId: string, imageCount: number): void {
     if (this.currentSlideIndices[contentId] > 0) {
